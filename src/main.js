cd1dfbbdcb1786fc443591a65b540119b306c8a5
qq534759298 <534759298@qq.com>
2019-02-16 22:00:18 +0800

    登录-发送请求

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/base.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 
@@ -14,6 +18,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
